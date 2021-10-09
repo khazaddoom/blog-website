a36725ffa41a5873318697405eceadd309460075
khazaddoom <31121986g@gmail.com>
2021-10-09 09:08:30 +0530

    fix to mobile menu header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 const menuHandler = document.getElementById("menu");
 const mainMenu = document.getElementById("main-menu");
+const mainContent = document.getElementById("main-content");
+
 menuHandler.addEventListener("click", function() {
     mainMenu.classList.toggle("hide")
+    mainContent.classList.toggle("hide");
 })
