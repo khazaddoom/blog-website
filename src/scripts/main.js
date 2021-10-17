f28ba73829a9cc72f71ab092d9521e13b410fbd0
khazaddoom <31121986g@gmail.com>
2021-10-17 21:40:06 +0530

    all styles done
    TODO: mobile side menu icon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,18 @@
 const menuHandler = document.getElementById("menu");
 const mainMenu = document.getElementById("main-menu");
 const mainContent = document.getElementById("main-content");
+const body = document.getElementById("desktop-full")
+const desktopMenu = document.getElementById("desktop-hamburger-menu");
 
 menuHandler.addEventListener("click", function() {
     mainMenu.classList.toggle("hide")
     mainMenu.style.width = "80%";
+})
+
+body.addEventListener("click", function() {
+    mainMenu.classList.toggle("hide")
+})
+
+desktopMenu.addEventListener("click", function() {
+    mainMenu.style.display = "none !important";
 })
